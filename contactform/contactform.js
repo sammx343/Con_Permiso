35c265913382e13a062db029b554a48026b2d95a
Alex Pico <54768905+alexpicoa@users.noreply.github.com>
2019-09-06 15:37:48 -0500

    Update contactform.js

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -92,7 +92,7 @@ jQuery(document).ready(function($) {
     else var str = $(this).serialize();
     $.ajax({
       type: "POST",
-      url: "contactform.php",
+      url: "contactfrom/contactform.php",
       data: str,
       success: function(msg) {
         // alert(msg);
